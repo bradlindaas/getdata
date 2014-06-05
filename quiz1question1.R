3840d2d1b5989cd955670f54d0e0cae86ee852d2
Brad Lindaas <brad@lindaas.org>
2014-06-05 10:03:56 -0500

    First commit

---
+++ quiz1question1.R
@@ -0,0 +1,13 @@
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+
+if (!file.exists("data")) {
+  dir.create("data")
+}
+
+if (!file.exists("./data/quiz1question1.csv")) {
+  download.file(fileURL, destfile = "./data/quiz1question1.csv", method="curl")
+}
+
+question1 <- read.table("./data/quiz1question1.csv", sep=",", header=TRUE)
+
+nrow(subset(question1, VAL==24))
