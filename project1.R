1a4dd3983db9f614f75d7a4fb8f451499595d614
Brad Lindaas <brad@lindaas.org>
2014-06-20 09:35:48 -0500

    finished all functions and added comments

--- project1.R
+++ project1.R
@@ -1,5 +1,5 @@
+
 library(reshape2)
-library(data.table)
 
 dataDir <- "/home/rstudio/largedata/"
 dataFileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
@@ -15,9 +15,12 @@ if (!file.exists(paste(dataDir, "UCI HAR Dataset/", sep=""))) {
 }
 
 getFeatureLabels <- function () {
-    ## Part 4, Appropriately labels the data set with descriptive variable names.
-    ## It is possible these are not descriptive?
-    feature_labels <- as.character(read.table(paste(dataDir, "UCI HAR Dataset/features.txt", sep=""), header=F)$V2)
+    ## getFeatureLabels() has no arguments, as there is only one features.txt file
+    ## the function extracts the feature labels from the "features.txt" file
+    ## these values will makeup the column names of the dataset
+    ## the function edits the labels to be more descriptive according to Part 4 
+    ## The function returns a character vector
+    feature_labels <- as.character(read.table(paste(dataDir, "UCI HAR Dataset/", "features.txt", sep=""), header=F)$V2)
     feature_labels <- gsub("\\(", "", feature_labels)
     feature_labels <- gsub("\\)", "", feature_labels)
     feature_labels <- gsub("\\,", "", feature_labels)
@@ -25,37 +28,59 @@ getFeatureLabels <- function () {
     return (feature_labels)
 }
 
-getActivityData <- function (x) {
-    ## Part 3, Uses descriptive activity names to name the activities in the data set
-    activity <- as.factor(read.table(paste(dataDir, "UCI HAR Dataset/", x, "/y_", x, ".txt", sep=""), header=F)$V1)
+getActivityData <- function (dir) {
+    ## getActivityData() takes one argument (dir) that specifies either "train" or "test" 
+    ## the function reads the specific Activty file to grab the specified actvity code
+    ## the function translates the code to the friendly name found in activity_labels.txt
+    ## as specified in Part 3, these values will makeup the column 'activity' of the dataset
+    ## The function returns a factor
+    activity <- as.factor(read.table(paste(dataDir, "UCI HAR Dataset/", dir, "/y_", dir, ".txt", sep=""), header=F)$V1)
     activity_labels <- read.table(paste(dataDir, "UCI HAR Dataset/activity_labels.txt", sep=""), header=F)
     activity <- sapply(activity, function(y) activity_labels[y,2])
     return (activity)
 }
 
-getSubjectData <- function(x) {
-    subjects <- as.character(read.table(paste(dataDir, "UCI HAR Dataset/", x, "/subject_", x,".txt", sep=""), header=F)$V1)
+getSubjectData <- function(dir) {
+    ## getSubjectData() takes one argument (dir) that specifies either "train" or "test" 
+    ## the function reads the specific Subject file to grab the specific subject of the observation
+    ## The function returns a numeric vector
+    subjects <- as.character(read.table(paste(dataDir, "UCI HAR Dataset/", dir, "/subject_", dir,".txt", sep=""), header=F)$V1)
     return(as.numeric(subjects))
 }
 
-readDataFile <- function(x) {
-    dataFile <- read.table(paste(dataDir, "UCI HAR Dataset/", x, "/X_", x,".txt", sep=""), header=F, colClasses="numeric")
-    #dataFile <- as.data.table(dataFile)
+readDataFile <- function(dir) {
+    ## readDataFile() takes one argument (dir) that specifies either "train" or "test" 
+    ## the function reads the specific file into a data.frame
+    ## This function was created to separate reading the files from constructing the data.frame
+    ## in the possiblity that I need to adjust the parameters of the read
+    ## The function returns a data frame
+    dataFile <- read.table(paste(dataDir, "UCI HAR Dataset/", dir, "/X_", dir,".txt", sep=""), header=F, colClasses="numeric")
     return(dataFile)
 }    
 
 getXData <- function(dir) {
+    ## getXData() takes one argument (dir) that specifies either "train" or "test" 
+    ## This function utilizes all the helper functions to construct the data frame
+    ## it calls the readDataFile helper function to grab the core data
+    ## then it cbinds the subject and activity columns using those helper functions
+    ## finally it sets the column names to the descriptive names using the helper functions
+    ## The function returns a data frame
     XData <- readDataFile(dir)
     XData <- cbind(getSubjectData(dir), getActivityData(dir),XData)
-    ## Part 3, Uses descriptive activity names to name the activities in the data set
-    setnames(XData, c("subject", "activity", getFeatureLabels()))
+    return(setnames(XData, c("subject", "activity", getFeatureLabels())))
 }
 
-## Part 1, Merge the training and the test sets to create one data set.
+## Part 1, Merge the train and the test sets to create one data set.
 mergedData <- rbind(getXData("test"), getXData("train"))
 
-## Part 2, Extracts only the measurements on the mean and standard deviation for each measurement.
+## Part 2, Extract only the measurements on the mean and standard deviation for each measurement.
+## Part 3, Use descriptive activity names to name the activities in the data set
+## part 4, Appropriately label the data set with descriptive variable names. 
 tidyData <- mergedData[,grepl("subject|activity|mean|std", names(mergedData))]
 
-## Part 5, Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-# these is where I use reshape2
+## Part 5, Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidyMelt <- melt(tidyData, id=c(1:2), measure.vars=c(3:81))
+averageTidyData<-dcast(tidyMelt, subject + activity ~ variable, mean)
+
+## Export the final tidy data file to CSV to upload with submission
+write.csv(averageTidyData, paste(dataDir, "part5.txt", sep=""), row.names=F)
